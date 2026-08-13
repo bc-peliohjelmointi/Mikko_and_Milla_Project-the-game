bb3d16d2756e3d32a78c4a3c7a0457ac9bfe7a40
Savelius Milla <s2401247@edu.bc.fi>
2026-08-13 10:18:01 +0300

    player skripti tehty
    KÄYTTÄÄ INPUT HANDLING

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class playerMovement : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize(); //normalses diagonal movement move so its the same speed as the other movements
+    }
+
+    //used for physics
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(movement.x * speed, movement.y * speed);
     }
 }
