30ac26c3757a3e3df809838f8d5c46e05b5a2e04
Savelius Milla <s2401247@edu.bc.fi>
2026-08-18 10:12:36 +0300

    collectable script valmis
    collectable script on pelaajassa

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem.Controls;
+using TMPro;
 
 public class playerMovement : MonoBehaviour
 {
@@ -7,12 +8,20 @@ public class playerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movement;
 
-    private int points = 0;
+    private int points;
+
+    public TextMeshProUGUI pointsText;
+    public GameObject winText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        points = 0; //set points to 0
+        SetCountText(); //activate the points text
+
+        //turn off win text at the start of the game
+        winText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -30,12 +39,34 @@ public class playerMovement : MonoBehaviour
         rb.linearVelocity = new Vector2(movement.x * speed, movement.y * speed);
     }
 
+    /// <summary>
+    /// checks if the player hits a collectable
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //does the object have the same tag?
         if (collision.gameObject.CompareTag("PickUp"))
         {
+            //collectable disapears
             collision.gameObject.SetActive(false);
+            points++;
+            SetCountText();
+        }
+    }
 
+    /// <summary>
+    /// show points (and win text)
+    /// </summary>
+    private void SetCountText()
+    {
+        pointsText.text = "Points: " + points.ToString();
+
+        //win if all points are collected
+        if (points >= 9)
+        {
+            //show win text
+            winText.SetActive(true);
         }
     }
 }
